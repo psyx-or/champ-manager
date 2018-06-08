050733d499e5aaf4bb14cbc70aed885b25d023af
dsenmart <didier.senmartin@soprasteria.com>
2018-06-08 18:12:08 +0200

    Prefixage des tables

---
+++ src/EventSubscriber/TablePrefixSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\Common\EventSubscriber;
+
+class TablePrefixSubscriber implements EventSubscriber
+{
+	private $prefix = '';
+
+    public function __construct(string $prefix)
+    {
+        $this->prefix = $prefix;
+	}
+	
+    public function getSubscribedEvents()
+    {
+        return [
+           'loadClassMetadata'
+        ];
+    }
+
+	public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $classMetadata = $eventArgs->getClassMetadata();
+
+        if (!$classMetadata->isInheritanceTypeSingleTable() || $classMetadata->getName() === $classMetadata->rootEntityName) {
+            $classMetadata->setPrimaryTable([
+                'name' => $this->prefix . $classMetadata->getTableName()
+            ]);
+        }
+
+        foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+            if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY && $mapping['isOwningSide']) {
+                $mappedTableName = $mapping['joinTable']['name'];
+                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
+            }
+        }
+    }
+}
